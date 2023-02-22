e2417c93d745150319d50e0bcd5b270a56ae50cc
Stv-devl <lansadstevan@gmail.com>
2023-02-22 18:28:06 +0700

    cleaning document

--- Src/JS/modal.js
+++ Src/JS/modal.js
@@ -7,17 +7,26 @@ function editNav() {
   }
 }
 
-// DOM Elements
+// Fonctions
 const modalbg = document.querySelector(".bground");
+const cross = document.querySelector(".close");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
+//adeventlistener
+
+// launch modal event when click on btn "Je m'inscris" (effacer for each? je vois un seul boutton... a voir)
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+//close the modale when click on the cross
+cross.addEventListener("click", closeModal);
